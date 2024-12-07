c7c271452fba9829d72728e69fb5c19d51607ee5
celll1 <cellione06@gmail.com>
2024-12-07 14:17:21 +0900

    Update ModelSetupDiffusionLossMixin.py

--- modules/modelSetup/mixin/ModelSetupDiffusionLossMixin.py
+++ modules/modelSetup/mixin/ModelSetupDiffusionLossMixin.py
@@ -329,6 +329,30 @@ class ModelSetupDiffusionLossMixin(metaclass=ABCMeta):
     ) -> Tensor:
         return self.__sigmas[timesteps].to(device=device)
 
+    def __logit_normal_weight(
+        self,
+        config: TrainConfig,
+        timesteps: Tensor,
+        device: torch.device,
+    ) -> Tensor:
+        x = self.__sigmas[timesteps]
+
+        # x = torch.clamp(x, min=1e-5, max=1-1e-5)
+
+        logit_x = torch.log(x / (1 - x))
+
+        mean = config.logit_normal_mean
+        std = config.logit_normal_std
+
+        # 1/std√2pi・1/x(1-x)・e^(-(logit(x)-mean)^2/2s^2)
+        norm_const = 1.0 / (std * torch.sqrt(torch.tensor(2.0 * torch.pi)))
+        jacobian = 1.0 / (x * (1.0 - x))
+        exp_term = torch.exp(-0.5 * ((logit_x - mean) / std) ** 2)
+
+        weight = norm_const * jacobian * exp_term
+
+        return weight.to(device=device)
+
     def _diffusion_losses(
             self,
             batch: dict,
@@ -410,14 +434,6 @@ class ModelSetupDiffusionLossMixin(metaclass=ABCMeta):
             else:
                 losses = self.__unmasked_losses(batch, data, config)
 
-        # apply loss weighting
-        if sigmas is not None and 'timestep' in data:
-            match config.loss_weight_fn:
-                case LossWeight.SIGMA:
-                    losses *= self.__sigma_weight(sigmas.flip(0), config, data['timestep'])
-                case LossWeight.LOGIT_NORMAL:
-                    losses *= self.__logit_normal_weight(sigmas.flip(0), config, data['timestep'])
-
         # Scale Losses by Batch and/or GA (if enabled)
         losses = losses * batch_size_scale * gradient_accumulation_steps_scale
 
@@ -428,5 +444,7 @@ class ModelSetupDiffusionLossMixin(metaclass=ABCMeta):
             match config.loss_weight_fn:
                 case LossWeight.SIGMA:
                     losses *= self.__sigma_loss_weight(data['timestep'], losses.device)
+                case LossWeight.LOGIT_NORMAL:
+                    losses *= self.__logit_normal_weight(config, data['timestep'], losses.device)
 
         return losses
